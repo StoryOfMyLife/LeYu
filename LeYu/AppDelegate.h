fb839424fb591f2ed53e05a90436009f6b9fdfd7
liuty <liutingyong@bytedance.com>
2015-09-15 23:32:17 +0800

    Initial Commit

---
+++ LeYu/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LeYu
+//
+//  Created by 刘廷勇 on 15/9/15.
+//  Copyright (c) 2015年 liuty. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LeYu/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LeYu
+//
+//  Created by 刘廷勇 on 15/9/15.
+//  Copyright (c) 2015年 liuty. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
